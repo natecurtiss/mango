c3cbfbbd06beee130afeb5203f6b111dc6e68108
natecurtiss <n8dev.cs@gmail.com>
2022-10-21 14:39:55 -0400

    Refactor Spritesheet

--- src/com/natecurtiss/mango/Spritesheet.java
+++ src/com/natecurtiss/mango/Spritesheet.java
@@ -4,16 +4,16 @@ import java.awt.image.BufferedImage;
 import java.util.LinkedList;
 
 public class Spritesheet {
+    private final LinkedList<Image> imgs = new LinkedList<>();
+
     private LinkedList<Image> getImages(BufferedImage img, int tileSize) {
         var width = img.getWidth() / tileSize;
         var height = img.getHeight() / tileSize;
 
-        LinkedList<Image> imgs = new LinkedList<>();
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
+        imgs.clear();
+        for (int i = 0; i < height; i++)
+            for (int j = 0; j < width; j++)
                 imgs.add(img.getSubimage(i * tileSize, j * tileSize, tileSize, tileSize));
-            }
-        }
         return imgs;
     }
 }
